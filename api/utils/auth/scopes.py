84fd910401cb9e4412af0f29de52fdf62676092f
Too-Zestyy <violet.steers.dev@gmail.com>
2026-03-08 12:52:35 +0000

    change: rework OAuth Scope mechanism for easier deserialisation by future middleware

--- api/utils/auth/scopes.py
+++ api/utils/auth/scopes.py
@@ -19,41 +19,37 @@ class ScopeAccessLevel(StrEnum):
         return ':'.join((scope_name, self.value))
 
 
-# TODO: Alternative to method in ``ScopeManager``: https://stackoverflow.com/questions/70852331/how-to-define-class-attributes-after-inheriting-pydantics-basemodel#71664301
-#  Use this as a more concrete way of parsing expected Scopes, migrate multiple scope per field management to this class
 class DBRecordScopeField(BaseModel, ABC):
+    """
+    Encapsulates applicable database-related scopes for a single type of content.
+    Used by the API to determine whether a user can read, write or delete certain content.
+    """
     FieldName: ClassVar[str] = 'TODO'
 
     # access_level: ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
-    read_access:   bool
-    write_access:  bool
-    delete_access: bool
+    read_access:   bool = False
+    write_access:  bool = False
+    delete_access: bool = False
 
     def get_scope_value(self) -> str | None:
         if not any((self.read_access, self.write_access, self.delete_access)):
             return None
         else:
-            access_levels_string = \
-            f'{'r' if self.read_access else ''}'
-            f'{'w' if self.write_access else ''}'
-            f'{'d' if self.delete_access else ''}'
+            access_levels_string = f'{'r' if self.read_access else ''}{'w' if self.write_access else ''}{'d' if self.delete_access else ''}'
             return f'{self.FieldName}:{access_levels_string}'
     
-    @classmethod
-    def try_from_string(cls, scope_string: str):
+    def try_from_string(self, scope_string: str) -> None:
         """
-        Attempts to look for a valid string representation of this scope, and returns it if available.
+        Attempts to look for a valid string representation of this scope, and mutates itself to match it if available.
         
         Args:
             scope_string (str): The stringified scope to search.
 
         Raises:
-            ValueError: _description_
-            ValueError: _description_
-            ValueError: _description_
+            ValueError: If the scope is not in a valid format to be deserialised.
 
         Returns:
-            _type_: The deserialised scopes represented by `scope_string`
+            _type_: The deserialised scopes represented by `scope_string`.
         """
         split = scope_string.split(':')
         
@@ -62,41 +58,37 @@ class DBRecordScopeField(BaseModel, ABC):
         
         if len(split) != 2:
             raise ValueError('not a valid scope string')
-        if scope_type != cls.FieldName:
+        if scope_type != self.FieldName:
             raise ValueError('scope string not for this scope')
         if len(scopes) > 3 or len(scopes) < 1:
             raise ValueError('no scopes provided in string')
         
-        return cls(
-            read_access='r' in scopes,
-            write_access='w' in scopes,
-            delete_access='d' in scopes
-        )
+        self.read_access = 'r' in scopes
+        self.write_access = 'w' in scopes
+        self.delete_access = 'd' in scopes
         
-    @classmethod
-    def try_from_token_scope_string(cls, token_scope_string: str):
+        return None
+        
+    def try_from_token_scope_string(self, token_scope_string: str) -> None:
         """
         A shorthand to simplify searching for a scope within a set of scopes provided by a JWT.
 
         Args:
             token_scope_string (str): The set of scopes to search within.
 
-        Raises:
-            ValueError: _description_
-
         Returns:
-            _type_: _description_
+            _type_: The deserialised scopes of the group represented by `token_scope_string`.
         """
         
         scopes = token_scope_string.split(' ')
         
         for scope in scopes:
             try:
-                return cls.try_from_string(scope)
+                return self.try_from_string(scope)
             except Exception:
                 pass
         
-        raise ValueError('scope not found')
+        # raise ValueError('scope not found')
     
 class SongScopeField(DBRecordScopeField):
     FieldName: ClassVar[str] = 'song'
@@ -108,7 +100,7 @@ class ArtistScopeField(DBRecordScopeField):
     FieldName: ClassVar[str] = 'artist'
     
 class ChartCreatorScopeField(DBRecordScopeField):
-    FieldName: ClassVar[str] = 'chart_creator'
+    FieldName: ClassVar[str] = 'creator'
     
 class GenreScopeField(DBRecordScopeField):
     FieldName: ClassVar[str] = 'genre'
@@ -131,58 +123,47 @@ class ScopeManager(BaseModel):
     their own scope permissions, and ``admin`` is equivalent ao all permissions in all areas.
     """
 
-    song_access:          ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
-    chart_access:         ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
-    artist_access:        ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
-    chart_creator_access: ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
-    genre_access:         ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
-    sdt_blob_access:      ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
-    audio_blob_access:    ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
-    video_blob_access:    ScopeAccessLevel | set[ScopeAccessLevel] = ScopeAccessLevel.none
+    song_access:          SongScopeField         = SongScopeField()
+    chart_access:         ChartScopeField        = ChartScopeField()
+    artist_access:        ArtistScopeField       = ArtistScopeField()
+    chart_creator_access: ChartCreatorScopeField = ChartCreatorScopeField()
+    genre_access:         GenreScopeField        = GenreScopeField()
+    sdt_blob_access:      SdtBlobScopeField      = SdtBlobScopeField()
+    audio_blob_access:    AudioScopeField        = AudioScopeField()
+    video_blob_access:    VideoScopeField        = VideoScopeField()
 
-    cross_edit_access: bool = False
-    admin: bool = False
-
-    # TODO: Return a field based on key string? Abstract keys into another class?
-    #  If the first solution, migrate ``access_keys`` in string constructor to use this function
-    #  Alternatively move
-    def field_from_key(self, key: str):
-        ...
+    cross_edit_access:    bool                   = False
+    admin:                bool                   = False
+    
+    def update_from_token_string(self, token_scopes: str):
+        
+        # While the split isn't necessary as of writing, doing so now will prevent an accidental collision 
+        # causing unwarranted permissions being given to users.
+        seperate_scopes = token_scopes.split(' ')
+        if 'admin' in seperate_scopes:
+            self.admin = True
+        if 'xedit' in seperate_scopes:
+            self.cross_edit_access = True
+        
+        # Dynamically get all ``DBRecordScopeField``s, ensuring inclusion of any future fields as they are added
+        for field_name, _ in filter(lambda f: issubclass(f[1].annotation, DBRecordScopeField), self.__pydantic_fields__.items()):
+            self.__getattribute__(field_name).try_from_token_scope_string(token_scopes)
+            
 
     def __str__(self):
         if self.admin:
             return 'admin'
+        
+        access_fields = []
+        
+        for field_name, _ in filter(lambda f: issubclass(f[1].annotation, DBRecordScopeField), self.__pydantic_fields__.items()):
+            stringified_scope = self.__getattribute__(field_name).get_scope_value()
+            if stringified_scope is not None:
+                access_fields.append(stringified_scope)
+        
+        scopes = ' '.join(access_fields)
 
-        else:
-            access_keys = {
-                'song': self.song_access,
-                'chart': self.chart_access,
-                'artist': self.artist_access,
-                'creator': self.chart_creator_access,
-                'genre': self.genre_access,
-                'sdt': self.sdt_blob_access,
-                'audio': self.audio_blob_access,
-                'video': self.video_blob_access,
-            }
+        if self.cross_edit_access:
+            scopes += ' xedit'  # Probably faster than ' '.join()
 
-            access_names = []
-
-            for access_name, access_level in access_keys.items():
-                if isinstance(access_level, set):
-                    if ScopeAccessLevel.all in access_level:
-                        access_names.append(ScopeAccessLevel.all.scope_stringify(access_name))
-                    elif ScopeAccessLevel.none not in access_level:
-                        access_names.append(
-                            ' '.join((level.scope_stringify(access_name) for level in access_level))
-                            # ':'.join((access_name, access_level.value))
-                        )
-                else:
-                    if access_level != ScopeAccessLevel.none:
-                        access_names.append(access_level.scope_stringify(access_name))
-
-            scopes = ' '.join(access_names)
-
-            if self.cross_edit_access:
-                scopes += ' xedit'  # Probably faster than ' '.join()
-
-            return scopes
+        return scopes

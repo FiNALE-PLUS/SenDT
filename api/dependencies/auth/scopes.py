ee406e5c44a0c75f2ebff422ad10ce3e5a5e784d
Too-Zestyy <violet.steers.dev@gmail.com>
2026-03-17 01:35:49 +0000

    feat: add full login flow with 2FA
    chore: correct access token validity period

--- api/dependencies/auth/scopes.py
+++ api/dependencies/auth/scopes.py
@@ -1,9 +1,10 @@
 from api.utils.auth.scopes.fields.db_access import ChartScopeField, DBReadSubScope, SongScopeField
-from api.utils.auth.scopes.fields.totp_access import TOTPScopeField, TOTPVerifySubScope
+from api.utils.auth.scopes.fields.totp_access import TOTPScopeField, TOTPTokenSubScope, TOTPVerifySubScope
 from api.utils.auth.scopes.scope_manager import ScopeManager
 
 
 two_factor_setup_verification_scope_manager = ScopeManager(totp_access=TOTPScopeField(user_setup_verification_access=TOTPVerifySubScope(granted=True)))
+two_factor_access_request_token_scope_manager = ScopeManager(totp_access=TOTPScopeField(authenticate_access_token_access=TOTPTokenSubScope(granted=True)))
 
 s_test = ScopeManager(
     song_access=SongScopeField(read_access=DBReadSubScope(granted=True)),
